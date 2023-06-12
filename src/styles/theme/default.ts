1b20e212bd28bc04bce87d5a5671cee4d00b3baa
Lucas <lucasleitet@gmail.com>
2023-06-12 15:52:02 -0300

    coffe-deliver star configurations

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,42 @@
+export const defaultTheme = {
+  colors: {
+    "base-white": "#FFFFFF",
+    "base-background": "#FAFAFA",
+    "base-card": "#F3F2F2",
+    "base-input": "#EDEDED",
+    "base-button": "#E6E5E5",
+    "base-hover": "#D7D5D5",
+    "base-label": "#8d8686",
+    "base-text": "#574F4D",
+    "base-subtitle": "#403937",
+    "base-title": "#272221",
+    "base-error": "#F84747",
+    "brand- yellow-light": "#F1E9C9",
+    "brand- yellow": "#DBAC2C",
+    "brand- yellow-dark": "#C47F17",
+    "brand-purple-light": "#EBE5F9",
+    "brand-purple": "#8047F8",
+    "brand-purple-dark": "#4B2995"
+  },
+  textSizes: {
+    "title-title-XL": "3rem",
+    "title-title-L": "2rem",
+    "title-title-M": "1.5rem",
+    "title-title-S": "1.2rem",
+    "title-title-XS": "1.125rem",
+    "text-regular-L": "1.25rem",
+    "text-bold-L": "1.25rem",
+    "text-regular-M": "1rem",
+    "text-bold-M": "1rem",
+    "text-regular-S": "0.875rem",
+    "text-bold-S": "0.75rem",
+    "componentes-tag": "0.625rem",
+    "componentes-button-G": "0.875rem",
+    "componentes-button-S": "0.75rem"
+  },
+  fonts: {
+    regular: "'Roboto'",
+    title: "'Baloo 2'"
+
+  },
+}; 
