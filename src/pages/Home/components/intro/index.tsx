ed76e7c2ed94406825cccb8e60448c1049fdb461
Lucas <lucasleitet@gmail.com>
2023-06-20 13:20:44 -0300

    add coffe delivery intro

---
+++ src/pages/Home/components/intro/index.tsx
@@ -0,0 +1,24 @@
+import introImg from "../../../../assests/intro-img.svg";
+import { IntroContainer, IntroContent, IntroTitle, RegularText } from "./styles";
+
+export function Intro() {
+  return (
+    <IntroContainer>
+      <IntroContent className="container">
+        <div>
+          <section>
+            <IntroTitle size="XL">
+              Encontre o café perfeito para qualquer hora do dia
+            </IntroTitle>
+            <RegularText size="L">
+              Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+            </RegularText>
+          </section>
+        </div>
+        <img src={introImg} />
+
+      </IntroContent>
+    </IntroContainer>
+  )
+}
+
---
+++ src/pages/Home/components/intro/styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+
+export const IntroContainer = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 34rem;
+  background: red;
+`;
+export const IntroContent= styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 3.5rem;
+  background: pink;
+
+`;
+
+interface TitleTextProps {
+  color?: "title" | "subtitle" | "text";
+  size?: "XL" | "L" | "M" | "S" | "XS"; 
+  weight?: string | number;
+}
+
+export const TitleText = styled.h1<TitleTextProps>`
+  color: ${({theme, color}) => theme.colors[`base-${color ?? "title" }`]};
+  font-size: ${({theme, size}) => theme.textSizes[`title-title-${size ?? "M"}`]};
+  font-family: ${({theme}) => theme.fonts.title};
+  line-height: 130%;
+  font-weight: ${({weight}) => weight ?? 800 }
+`;
+
+interface RegularTextProps {
+  color?: "text" | "subtitle" | "label";
+  size?: "L" | "M" | "S"; 
+  weight?: string | number;
+}
+
+export const RegularText = styled.p<RegularTextProps>`
+  color: ${({theme, color}) => theme.colors[`base-${color ?? "title" }`]};
+  font-size: ${({theme, size}) => theme.textSizes[`text-regular-${size ?? "M"}`]};
+  font-family: ${({theme}) => theme.fonts.regular};
+  line-height: 130%;
+  font-weight: ${({weight}) => weight ?? 400 }
+`;
+
+export const IntroTitle = styled(TitleText)`
+margin-bottom:1rem;
+`;
+  
+
+
