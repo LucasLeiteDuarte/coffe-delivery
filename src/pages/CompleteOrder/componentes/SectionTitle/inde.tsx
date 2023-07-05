db0c4564d414bac2c03a7a99de27d645d4f3518a
Lucas <lucasleitet@gmail.com>
2023-07-05 18:23:28 -0300

    sectioTitle creat

---
+++ src/pages/CompleteOrder/componentes/SectionTitle/inde.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface SectionTitleProps {
+  title: string;
+  subtitle: string;
+  icon: ReactNode;
+}
+
+export function SectionTitle({ title, subtitle, icon }: SectionTitleProps) {
+  return (
+
+  )
+}
