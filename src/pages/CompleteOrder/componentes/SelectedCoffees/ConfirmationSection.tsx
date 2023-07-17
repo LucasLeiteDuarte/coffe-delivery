d74205b9c9c14c7339a007eb15fda9eb4073b439
Lucas <lucasleitet@gmail.com>
2023-07-17 17:57:31 -0300

    total

--- src/pages/CompleteOrder/componentes/SelectedCoffees/ConfirmationSection.tsx
+++ src/pages/CompleteOrder/componentes/SelectedCoffees/ConfirmationSection.tsx
@@ -8,6 +8,18 @@ export function ConfirmationSection() {
         <RegularText size="S">Total de itens</RegularText>
         <RegularText>R$ 9,90</RegularText>
       </div>
+      <div>
+        <RegularText size="S">Entrega</RegularText>
+        <RegularText>R$ 3,50</RegularText>
+      </div>
+      <div>
+        <RegularText weight="700" color="subtitle" size="L">
+          Total
+        </RegularText>
+        <RegularText weight="700" color="subtitle" size="L">
+          R$ 29,90
+        </RegularText>
+      </div>
     </ConfirmationSectionContainer>
   )
 }
